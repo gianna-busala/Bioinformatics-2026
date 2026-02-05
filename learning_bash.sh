0ba2cb722699a3a5d38b7c0dc5a2c0678f85bbe9
Gianna <gianna.busala@wmich.edu>
2026-02-05 15:31:31 -0500

    Add files via upload
    Assignment 1

---
+++ learning_bash.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+greeting="heyyy"
+yourname="Gianna"
+echo "$greeting $yourname !"
+echo "This script is helping you learn bash"
+mkdir /home/giannabusala/Desktop/bioinformatics
+mkdir /home/giannabusala/Desktop/bioinformatics/bash_scripts
+mv /home/giannabusala/Downloads/bash_template.txt /home/giannabusala/Desktop/bioinformatics/bash_scripts
+mv /home/giannabusala/Desktop/bioinformatics/bash_scripts/bash_template.txt /home/giannabusala/Desktop/bioinformatics/bash_scripts/bash_template.sh 
